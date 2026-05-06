6737164b67c924002b86f54fb049805d2dba340d
Vladislav-Ushakov-FIT-241 <chesmotrishotvernis@gmail.com>
2026-05-07 01:12:12 +0600

    add RegisterIoCDependencyMacroCommand and RegisterIoCDependencyMacroCommandTests

---
+++ SpaceBattle.Tests/RegisterIoCDependencyMacroCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using Moq;
+using System.Collections.Generic;
+using SpaceBattle;
+
+namespace SpaceBattle.Tests;
+
+public class RegisterIoCDependencyMacroCommandTests
+{
+    [Fact]
+    public void Execute_RegisterIoCDependencyMacroCommand_DependencyResolves()
+    {
+        var cmd1 = new Mock<ICommand>();
+        var cmd2 = new Mock<ICommand>();
+        var commands = new List<ICommand> { cmd1.Object, cmd2.Object };
+
+        new RegisterIoCDependencyMacroCommand().Execute();
+
+        var macro = Ioc.Resolve<ICommand>("Commands.Macro", commands);
+
+        Assert.NotNull(macro);
+        macro.Execute();
+
+        cmd1.Verify(c => c.Execute(), Times.Once());
+        cmd2.Verify(c => c.Execute(), Times.Once());
+    }
+}
