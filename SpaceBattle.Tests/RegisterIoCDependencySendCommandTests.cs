e5352773841cc1641c6e172c6600783a271368f5
SSnegovik <kudinovkostyan@yandex.ru>
2026-05-07 03:05:36 +0600

    git: add RegisterIoCDependencySendCommand and RegisterIoCDependencySendCommandTests

---
+++ SpaceBattle.Tests/RegisterIoCDependencySendCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using Xunit;
+using SpaceBattle;
+
+namespace SpaceBattle.Tests;
+
+public class RegisterIoCDependencySendCommandTests
+{
+    [Fact]
+    public void Execute_RegisterIoCDependencySendCommand_DependencyResolves()
+    {
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        new RegisterIoCDependencySendCommand().Execute();
+
+        var cmd = Ioc.Resolve<ICommand>("Commands.Send", new Mock<ICommand>().Object, mockReceiver.Object);
+
+        Assert.NotNull(cmd);
+    }
+}
---
+++ SpaceBattle.Tests/SendCommandTests.cs
@@ -0,0 +1,81 @@
+﻿using Moq;
+using Xunit;
+using SpaceBattle;
+
+namespace SpaceBattle.Tests;
+
+public class SendCommandTests
+{
+    [Fact]
+    public void SendCommand_PassesCommandToReceiver()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+        sendCommand.Execute();
+
+        mockReceiver.Verify(r => r.Receive(mockCommand.Object), Times.Once);
+    }
+
+    [Fact]
+    public void SendCommand_WhenReceiverFails_ThrowsException()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+        mockReceiver.Setup(r => r.Receive(It.IsAny<ICommand>())).Throws(new Exception());
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+
+        Assert.Throws<Exception>(() => sendCommand.Execute());
+    }
+
+    [Fact]
+    public void SendCommand_ShouldStoreCommandReference()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+        sendCommand.Execute();
+
+        mockReceiver.Verify(r => r.Receive(It.Is<ICommand>(c => c == mockCommand.Object)), Times.Once);
+    }
+
+    [Fact]
+    public void SendCommand_ShouldNotExecute()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+        sendCommand.Execute();
+
+        mockCommand.Verify(c => c.Execute(), Times.Never);
+    }
+
+    [Fact]
+    public void SendCommand_WithNullReceiver()
+    {
+        var mockCommand = new Mock<ICommand>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, null);
+
+        Assert.Throws<NullReferenceException>(() => sendCommand.Execute());
+    }
+
+    [Fact]
+    public void SendCommand_ExecuteMultipleCalls()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+
+        sendCommand.Execute();
+        sendCommand.Execute();
+        sendCommand.Execute();
+
+        mockReceiver.Verify(r => r.Receive(mockCommand.Object), Times.Exactly(3));
+    }
+}
