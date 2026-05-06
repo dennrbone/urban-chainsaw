d81c40b74dfe680a381dbe54ffa255fdc924f63d
SSnegovik <kudinovkostyan@yandex.ru>
2026-05-07 01:55:30 +0600

    git add new tests

--- SpaceBattle.Tests/RotateCommandTests.cs
+++ SpaceBattle.Tests/RotateCommandTests.cs
@@ -58,4 +58,66 @@ public class RotateCommandTests
 
         Assert.Throws<System.Exception>(() => command.Execute());
     }
+
+    [Fact]
+    public void Sum_RotateWithCoord()
+    {
+        var angle = new Angle(2, 8);
+        var angularVelocity = new Angle(2, 8);
+        var expectedAngle = new Angle(4, 8);
+
+        var rotatingObject = new Mock<IRotatingObject>();
+        rotatingObject.Setup(r => r.Angle).Returns(angle);
+        rotatingObject.Setup(r => r.AngularVelocity).Returns(angularVelocity);
+
+        var command = new RotateCommand(rotatingObject.Object);
+
+        command.Execute();
+
+        rotatingObject.VerifySet(r => r.Angle = It.Is<Angle>(a => a.Numerator == expectedAngle.Numerator), Times.Once);
+    }
+
+
+    [Fact]
+    public void Sum_Rotate_WithNormalization()
+    {
+        var angle = new Angle(6, 8);
+        var angularVelocity = new Angle(4, 8);
+        var expectedAngle = new Angle(2, 8);
+
+        var rotatingObject = new Mock<IRotatingObject>();
+        rotatingObject.Setup(r => r.Angle).Returns(angle);
+        rotatingObject.Setup(r => r.AngularVelocity).Returns(angularVelocity);
+
+        var command = new RotateCommand(rotatingObject.Object);
+
+        command.Execute();
+
+        rotatingObject.VerifySet(r => r.Angle = It.Is<Angle>(a => a.Numerator == expectedAngle.Numerator), Times.Once);
+    }
+
+    [Fact]
+    public void RotateCommand_ShouldInitializeCorrectly()
+    {
+        var rotatingObject = new Mock<IRotatingObject>();
+
+        var command = new RotateCommand(rotatingObject.Object);
+
+        Assert.NotNull(command);
+    }
+
+    [Fact]
+    public void Execute_ShouldCallGettersOnce()
+    {
+        var rotatingObject = new Mock<IRotatingObject>();
+        rotatingObject.Setup(r => r.Angle).Returns(new Angle(0, 8));
+        rotatingObject.Setup(r => r.AngularVelocity).Returns(new Angle(1, 8));
+
+        var command = new RotateCommand(rotatingObject.Object);
+
+        command.Execute();
+
+        rotatingObject.Verify(r => r.Angle, Times.Once);
+        rotatingObject.Verify(r => r.AngularVelocity, Times.Once);
+    }
 }
