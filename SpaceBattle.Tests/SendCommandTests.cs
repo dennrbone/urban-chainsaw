6d734f71b442adaed8caaf676a81c56c6ea518f4
SSnegovik <kudinovkostyan@yandex.ru>
2026-05-07 02:40:39 +0600

    git: add new tests

--- SpaceBattle.Tests/SendCommandTests.cs
+++ SpaceBattle.Tests/SendCommandTests.cs
@@ -29,4 +29,53 @@ public class SendCommandTests
 
         Assert.Throws<Exception>(() => sendCommand.Execute());
     }
+
+    [Fact]
+    public void SendCommand_ShouldStoreCommandReference()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+        sendCommand.Execute();
+
+        mockReceiver.Verify(r => r.Receive(It.Is<ICommand>(c => c == mockCommand.Object)), Times.Once);
+    }
+
+    [Fact]
+    public void SendCommand_ShouldNotExecute()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+        sendCommand.Execute();
+
+        mockCommand.Verify(c => c.Execute(), Times.Never);
+    }
+
+    [Fact]
+    public void SendCommand_WithNullReceiver()
+    {
+        var mockCommand = new Mock<ICommand>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, null);
+
+        Assert.Throws<NullReferenceException>(() => sendCommand.Execute());
+    }
+
+    [Fact]
+    public void SendCommand_ExecuteMultipleCalls()
+    {
+        var mockCommand = new Mock<ICommand>();
+        var mockReceiver = new Mock<ICommandReceiver>();
+
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
+
+        sendCommand.Execute();
+        sendCommand.Execute();
+        sendCommand.Execute();
+
+        mockReceiver.Verify(r => r.Receive(mockCommand.Object), Times.Exactly(3));
+    }
 }
