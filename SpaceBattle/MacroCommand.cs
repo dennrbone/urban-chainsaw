0200fb6a1f369fa8d195697e1479291bbadefa00
Vladislav-Ushakov-FIT-241 <chesmotrishotvernis@gmail.com>
2026-05-07 02:42:08 +0600

    add RegisterIoCDependencyMacroMoveRotate and RegisterIoCDependencyMacroMoveRotateTests

---
+++ SpaceBattle/MacroCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceBattle;
+
+public class MacroCommand : ICommand
+{
+    private readonly IEnumerable<ICommand> _commands;
+
+    public MacroCommand(IEnumerable<ICommand> commands) => _commands = commands;
+
+    public void Execute()
+    {
+        _commands.ToList().ForEach(c => c.Execute());
+    }
+}
